610679eb8e03ea509ae954d6d2a26bcfb2aa8dd7
Denmoth <denmoth8871top@gmail.com>
2025-11-24 07:31:18 +0200

    first commit

---
+++ src/main/java/net/denmoth/aso/ASO.java
@@ -0,0 +1,13 @@
+package net.denmoth.aso;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ASO.MOD_ID)
+public class ASO {
+
+    public static final String MOD_ID = "aso";
+
+    public ASO() {
+        System.out.println("[ASO] Ad Astra: Structures Overhaul loaded!");
+    }
+}
